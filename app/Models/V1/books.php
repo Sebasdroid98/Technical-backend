4b13fa9071e72a2dc240e0fd1498a49bd669c778
Sebastian Zamora <zam98qui@gmail.com>
2024-10-19 17:44:54 -0500

    Se finaliza la version 1 del api

---
+++ app/Models/V1/books.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class books extends Model
+{
+    use HasFactory;
+
+    /* Se define el nombre de la llave primaria */
+    protected $primaryKey = 'id_libro';
+
+    /**
+     * Definimos los atributos que podran se asignados masivamente
+     */
+    protected $fillable = [
+        'nombre_libro'
+    ];
+
+    /**
+     * Mutador para almacenar el nombre del libro en mayúsculas
+     */
+    public function setNombreLibroAttribute($value)
+    {
+        $this->attributes['nombre_libro'] = strtoupper($value);
+    }
+}
