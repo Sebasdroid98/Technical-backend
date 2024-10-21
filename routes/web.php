04a98656d5f3ab73257863e7c1065cb73105f623
SebastianZQ <zam98qui@gmail.com>
2024-10-21 18:45:00 -0500

    Se agrega ruta para ejecutar las migraciones

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migraciones ejecutadas con éxito.';
+})->name('ejecutar-migraciones');
